7b027dda57e29cb734c8c358b28bd898b9d167f4
rafifZayedV <rafif.zayed@vodafone>
2018-02-15 10:39:18 +0200

    deleted

--- Code/cache-service/src/main/java/vis/spain/cache/service/CacheService.java
+++
@@ -1,7 +0,0 @@
-package vis.spain.cache.service;
-
-import vis.spain.errorHandling.GeneralSystemFailureException;
-
-public interface CacheService {
-	void updateCache() throws GeneralSystemFailureException;
-}
