d512f75bf789442c029a44b83a51cf132f493e79
sherif <sherifahmed126@gmail.com>
2018-02-15 14:27:39 +0200

    finish catalog Cache Service

--- Code/jobs-service/src/main/java/vis/spain/cache/service/CacheServiceImpl.java
+++ Code/jobs-service/src/main/java/vis/spain/cache/service/CacheServiceImpl.java
@@ -1,7 +1,6 @@
 package vis.spain.cache.service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -20,10 +19,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
+import com.mongodb.BulkWriteResult;
 import com.mongodb.DBObject;
-import com.mongodb.Mongo;
-import com.mongodb.bulk.BulkWriteResult;
 
 import lombok.extern.slf4j.Slf4j;
 import vis.spain.errorHandling.GeneralSystemFailureException;
@@ -40,83 +37,45 @@ public class CacheServiceImpl implements CacheService {
 	private String catalogurl;
 	@Autowired
 	private MongoTemplate mongoTemplate;
-	
-	
-	@Autowired
-	private Mongo mongo;
-
 	private static final long ONE_HOUR = 60 * 60 * 1000L;
 
-	// @Scheduled(initialDelay = ONE_HOUR, fixedDelay = ONE_HOUR)
-	// @PostConstruct
+	@Scheduled(initialDelay = ONE_HOUR, fixedDelay = ONE_HOUR)
+	@PostConstruct
+	public void init() {
+		try {
+			updateCache();
+		} catch (Exception e) {
+			log.info("error occured in initialization " + e.getMessage());
+		}
+	}
+
 	public void updateCache() throws GeneralSystemFailureException {
-		// ServiceDataFromRP[] servicisList =
-		// restTemplate.getForObject(catalogurl, ServiceDataFromRP[].class);
-		ServiceDataFromRP[] servicisList = new ServiceDataFromRP[2];
-		servicisList[0] = new ServiceDataFromRP();
-		servicisList[0].setCode("aewr");
-		servicisList[0].setCategory("rafif");
+		try {
+			ServiceDataFromRP[] servicisList = restTemplate.getForObject(catalogurl, ServiceDataFromRP[].class);
+			if (servicisList == null)
+				throw new GeneralSystemFailureException(GeneralSystemFailureExceptionEnum.UNXPECTED_ERROR_OCCURED);
 
-		servicisList[1] = new ServiceDataFromRP();
-		servicisList[1].setCode("a111ewr");
-		servicisList[1].setCategory("445656");
-
-		
-		/*servicisList[2] = new ServiceDataFromRP();
-		servicisList[2].setCode("222");
-		servicisList[2].setCategory("sherif");*/
-		
-		/*
-		  DB db = mongo.getDB("vfes");
-
-		  // get a single collection
-		  DBCollection collection = db.getCollection("CatalogCache");
-		  new BasicDBObject("$set", toSet)
-		  
-		  
-		  
-		  // find type = vps , update all matched documents , clients value to 888
-		  BasicDBObject updateQuery = new BasicDBObject();
-		  updateQuery.append("$set", new BasicDBObject().append("clients", "888"));
-
-		  BasicDBObject searchQuery3 = new BasicDBObject();
-		  searchQuery3.append("type", "vps");
-
-		  collection.updateMulti(searchQuery3, updateQuery);
-		  	for (ServiceDataFromRP catalog : servicisList) {
-			DBObject dbObject = new BasicDBObject();
-			mongoTemplate.getConverter().write(catalog, dbObject);
-			Pair<Query, Update> pair = Pair.<Query, Update>of(Query.query(Criteria.where("code").is(catalog.getCode())),
-					Update.fromDBObject(dbObject));
-			catalogUpsert.add(pair);
-			codes.add(catalog.getCode());
-			
-		}
-		
-		  */
-		 
-	//	mongoTemplate.insert(Arrays.asList(servicisList), ServiceDataFromRP.class);
-		if (servicisList == null)
+			BulkOperations bulkOperations = mongoTemplate.bulkOps(BulkMode.UNORDERED, ServiceDataFromRP.class);
+			List<Pair<Query, Update>> catalogUpsert = new ArrayList<>();
+			List<String> codes = new ArrayList<>();
+			for (ServiceDataFromRP catalog : servicisList) {
+				DBObject dbObject = new BasicDBObject();
+				mongoTemplate.getConverter().write(catalog, dbObject);
+				Pair<Query, Update> pair = Pair.<Query, Update>of(
+						Query.query(Criteria.where("code").is(catalog.getCode())),
+						Update.fromDBObject(new BasicDBObject("$set", dbObject)));
+				catalogUpsert.add(pair);
+				codes.add(catalog.getCode());
+			}
+			bulkOperations.upsert(catalogUpsert);
+			bulkOperations.remove(Query.query(Criteria.where("code").nin(codes)));
+			BulkWriteResult result = bulkOperations.execute();
+			log.info("No of insert" + result.getInsertedCount());
+			log.info("No of modifiy" + result.getModifiedCount());
+			log.info("No of removed" + result.getRemovedCount());
+		} catch (Exception e) {
+			log.info(e.getMessage());
 			throw new GeneralSystemFailureException(GeneralSystemFailureExceptionEnum.UNXPECTED_ERROR_OCCURED);
-		// mongoTemplate.insert(Arrays.asList( servicisList), ServiceDataFromRP.class);
-
-		BulkOperations bulkOperations = mongoTemplate.bulkOps(BulkMode.UNORDERED, ServiceDataFromRP.class);
-		List<Pair<Query, Update>> catalogUpsert = new ArrayList<>();
-		List<String> codes = new ArrayList<>();
-		for (ServiceDataFromRP catalog : servicisList) {
-			DBObject dbObject = new BasicDBObject();
-			mongoTemplate.getConverter().write(catalog, dbObject);
-			Pair<Query, Update> pair = Pair.<Query, Update>of(Query.query(Criteria.where("code").is(catalog.getCode())),
-					Update.fromDBObject(new BasicDBObject("$set", dbObject) ));
-			catalogUpsert.add(pair);
-			codes.add(catalog.getCode());
 		}
-		bulkOperations.upsert(catalogUpsert);
-		bulkOperations.remove(Query.query(Criteria.where("code").nin(codes)));
-		com.mongodb.BulkWriteResult result = bulkOperations.execute();
-
-		log.info("No of insert" + result.getInsertedCount());
-		log.info("No of modifiy" + result.getModifiedCount());
-		log.info("No of removed" + result.getRemovedCount());
 	}
 }
